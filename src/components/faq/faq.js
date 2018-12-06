2160c0d817c824ee1db11d385196ce28064b1f37
Laura Lopez <laura-lop@hotmail.fr>
2018-12-06 22:09:44 +0100

    add faq file

---
+++ src/components/faq/faq.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Faq extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>F.A.Q</h2>
+            </div>
+        );
+    }
+}
+
+export default Faq;
