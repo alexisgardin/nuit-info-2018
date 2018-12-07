40aceced28b30978478f3b1f5b771d53098f4a48
Gregory Merlet <gregory.merlet@outlook.fr>
2018-12-07 07:18:19 +0100

    Adding la dégeulasserie de Seb

--- src/layouts/login/login.js
+++ src/layouts/login/login.js
@@ -6,26 +6,40 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import {NavLink} from 'react-router-dom';
 
+import './login.css'
+
 class Login extends React.Component {
 
   render() {
 
 	return (
-		<Card className="card">
+		<Card className="card loginCard">
 		  <CardContent>
 			<Typography className="title" color="textSecondary" style={{fontFamily: 'Calibri Light'}} gutterBottom>
 			  Accès sécurisé à ExplorationBoard
 			</Typography>
-			<Typography variant="h5" style={{fontFamily: 'Calibri Light'}} component="h2">
-			  Coucou
-			</Typography>
-			<Typography style={{fontFamily: 'Calibri Light'}} component="p">
-			  Voila
-			</Typography>
+
+              <div id="info" style={{display: "none"}}>
+              </div>
+              <div id="loading">
+                  Loading the model...
+              </div>
+              <div id='main' style={{display: "none"}}>
+                  <video id="video" playsInline style={{transform: "scaleX(-1)", display: "none"}}>
+                  </video>
+                  <canvas id="output"/>
+              </div>
+              <div className="lescads"></div>
+              <div className="cadenas" id="cadenas1"></div>
+              <div className="cadenas" id="cadenas2"></div>
+              <div className="cadenas" id="cadenas3"></div>
+              <div className="cadenas" id="cadenas4"></div>
+              <script src={require("./camera.js")}></script>
+
 		  </CardContent>
 		  <CardActions>
 			<NavLink to="/home" className="card-button">
-			  <Button size="small" color="primary" className="card-button">
+			  <Button id="authButton" size="small" color="primary" className="card-button">
 				Authentification
 			  </Button>
 			</NavLink>
